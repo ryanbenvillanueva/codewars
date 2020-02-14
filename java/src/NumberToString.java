d0d1f67e162b81935d6f5b09fadb89ed2649ca05
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-02-14 10:01:42 +0800

    Solve 8kyu kata - Convert a Number to a String! - NumberToString.java

---
+++ java/src/NumberToString.java
@@ -0,0 +1,10 @@
+public class NumberToString {
+    public static String numberToString(int num) {
+        return "" + num;
+    }
+
+    public static void main(String[] args) {
+        NumberToString nts = new NumberToString();
+        System.out.println(nts.numberToString(123));
+    }
+}
