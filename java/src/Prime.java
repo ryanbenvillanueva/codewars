1e5208a6140e9b2cab5d9dee402600d614bfc048
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-02-24 09:21:24 +0800

    Solve 6kyu kata - Is a Number Prime? - Prime.java

---
+++ java/src/Prime.java
@@ -0,0 +1,18 @@
+public class Prime {
+    public static boolean isPrime(int num) {
+
+        if (num <= 0 || num == 1) return false;
+
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(29));
+    }
+}
