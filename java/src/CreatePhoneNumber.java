32d450408d6d3a061cc702d203c1d58f9ea4883d
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-02-26 12:24:56 +0800

    Solve 6kyu kata - Create Phone Number - CreatePhoneNumber.java

---
+++ java/src/CreatePhoneNumber.java
@@ -0,0 +1,10 @@
+public class CreatePhoneNumber {
+    public static String createPhoneNumber(int[] n) {
+        return "(" + n[0] + n[1] + n[2] + ") " + n[3] + n[4] + n[5] + "-" + n[6] + n[7] + n[8] + n[9];
+    }
+
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(createPhoneNumber(array));
+    }
+}
