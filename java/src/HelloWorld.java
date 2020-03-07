7c849a2a991c1698fe8e12d2bb9eb62d5e5b133a
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-03-08 06:48:20 +0800

    Solve 8kyu kata - Function 1 - Hello World - HelloWorld.java

---
+++ java/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String greet() {
+        return "hello world!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
