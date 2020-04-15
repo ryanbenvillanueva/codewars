50fb0119c9ca2fb87a51b34427bc1a62db8ab519
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-04-15 10:44:10 +0800

    Solve 8kyu kata - Count odd numbers below n - OddCount.java

---
+++ java/src/OddCount.java
@@ -0,0 +1,9 @@
+public class OddCount {
+    public static int oddCount(int n) {
+        return n / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(oddCount(15));
+    }
+}
