a1ea2afc8688bae47f15d186aec5ec6b5baae61b
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-04-20 09:51:28 +0800

    Solve 8kyu kata - Area or Perimeter - AreaOrPerimeter.java

---
+++ java/src/AreaOrPerimeter.java
@@ -0,0 +1,9 @@
+public class AreaOrPerimeter {
+    public static int areaOrPerimeter(int a, int b) {
+        return a == b ? a * b : 2 * (a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areaOrPerimeter(4, 4));
+    }
+}
