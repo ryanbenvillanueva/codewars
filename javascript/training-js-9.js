12bcb7ceb6e23e0c98254a624aff3ff2f2f81a87
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-09-04 08:14:13 +0800

    Solve 8kyu kata - Training JS#9

---
+++ javascript/training-js-9.js
@@ -0,0 +1,14 @@
+function padIt(str,n){
+  let i = 1;
+  let even = '';
+  let odd = '';
+  while (i <= n) {
+    if (i % 2 === 0) {
+      even += '*';
+    } else {
+      odd += '*';
+    }
+    i++;
+  }
+  return odd + str + even;
+}
