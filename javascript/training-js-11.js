a93ed9d1adef973473c2f595320e8b61494c4a02
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-09-05 07:05:38 +0800

    Solve 8kyu kata - Training JS#11

---
+++ javascript/training-js-11.js
@@ -0,0 +1,16 @@
+function grabDoll(dolls) {
+  const bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+      bag.push(dolls[i]);
+    } else {
+      continue;
+    }
+
+    if (bag.length === 3) {
+      break;
+    }
+  }
+
+  return bag;
+}
