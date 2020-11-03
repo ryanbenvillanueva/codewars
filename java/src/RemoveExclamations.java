575ad4659765df26ac20d86c2d5c760b3e2357a0
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-11-03 10:05:04 +0800

    Solve 8kyu kata - Remove exclamation points from left to right

---
+++ java/src/RemoveExclamations.java
@@ -0,0 +1,12 @@
+public class RemoveExclamations {
+
+  public static String remove(String s, int n) {
+  
+      for (int i = 0; i < n; i++) {
+          s = s.replaceFirst("!", "");
+      }
+      
+      return s;
+  }
+  
+}
