88753f969efb2e649b07f52de025bfb37e60b1f3
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-11-07 08:28:59 +0800

    Solve 8kyu kata - Filtering even numbers

---
+++ java/src/FilteringEvenNumbers.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Kata13December {
+
+    public static List<Integer> filterOddNumber(List<Integer> listOfNumbers) {
+        List<Integer> newList = new ArrayList<Integer>();
+        
+        for (int i : listOfNumbers) {
+          if (i % 2 != 0) {
+            newList.add(i);
+          }
+        }
+        
+        return newList;
+    }
+
+}
