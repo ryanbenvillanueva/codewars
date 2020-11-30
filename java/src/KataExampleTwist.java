223834fcb2db2941ab21117d6a07c71b431c52a5
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-11-30 10:08:37 +0800

    Solve 8kyu kata - Kata example twist

---
+++ java/src/KataExampleTwist.java
@@ -0,0 +1,11 @@
+public class KataExampleTwist {
+    public static String[] kataExampleTwist() {
+        String[] websites = new String[1000];
+
+        for (int i = 0; i < websites.length; i++) {
+            websites[i] = "codewars";
+        }
+
+        return websites;
+    }
+}
