197fd276b03da14d672cd2ee2321f452e5699d95
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-12-04 09:34:48 +0800

    Solve 8kyu kata - Grasshopper - Array Mean

---
+++ java/src/ArrayMean.java
@@ -0,0 +1,12 @@
+public class ArrayMean {
+
+    public static int findAverage(int[] nums) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        return sum / nums.length;
+    }
+}
