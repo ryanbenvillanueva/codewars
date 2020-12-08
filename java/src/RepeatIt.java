390fada54b8eec79260e724de05438c7b9f177fc
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2020-12-08 15:08:10 +0800

    Solve 8kyu kata - Watermelon

--- java/src/RepeatIt.java
+++ java/src/RepeatIt.java
@@ -1,17 +1,18 @@
 public class RepeatIt {
 
-  public static String repeatString(final Object toRepeat, final int n) {
-    String str = "";
-    int i = 0;
+	public static String repeatString(final Object toRepeat, final int n) {
+		String str = "";
+		int i = 0;
 
-    if (toRepeat instanceof String) {
-      while (i < n) {
-        str += toRepeat;
-        i++;
-      } 
-    } else {
-        str = "Not a string";
-    }
-    
-    return str;
-  }
+		if (toRepeat instanceof String) {
+			while (i < n) {
+				str += toRepeat;
+				i++;
+			}
+		} else {
+			str = "Not a string";
+		}
+
+		return str;
+	}
+}
