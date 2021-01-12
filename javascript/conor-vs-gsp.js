12a1efa4ddaf3d128b7533ac423f6276fd279e9a
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2021-01-12 21:41:05 +0800

    Solve 8kyu kata - UFC conor vs gsp

---
+++ javascript/conor-vs-gsp.js
@@ -0,0 +1,7 @@
+const quote = function (fighter) {
+  if (fighter.toLowerCase() === "george saint pierre") {
+    return "I am not impressed by your performance.";
+  } else if (fighter.toLowerCase() === "conor mcgregor") {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+};
